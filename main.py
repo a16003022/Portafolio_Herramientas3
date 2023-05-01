3ca617742f5755dd84a8d133e57952d0aea53392
HeislerNarvaez <a16003022@alumnos.uady.mx>
2023-05-01 14:59:45 -0600

    subiendo cambios

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import streamlit as st
 import os
 
-# Obtener todos los archivos dentro de la carpeta "Pages" que terminen en .py
+# Obtener todas las páginas .py en la carpeta Pages
 pages = [f[:-3] for f in os.listdir("Pages") if f.endswith(".py")]
 
-# Crear una lista con el nombre de cada página
-page_names = [page.split(".")[0] for page in pages]
+# Agregar la página seleccionada a la barra lateral
+selection = st.sidebar.selectbox("Ir a", pages)
 
-# Mostrar una lista de páginas en el sidebar
-selected_page = st.sidebar.selectbox("Selecciona una página", page_names)
+# Importar la página seleccionada y ejecutarla
+page = __import__(f"Pages.{selection}")
+page.run()
 
-# Importar y mostrar la página seleccionada
-page_path = os.path.join("Pages", f"{selected_page}.py")
-page_module = __import__(f"Pages.{selected_page}")
-page_module.run()
