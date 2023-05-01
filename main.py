ec20f55fcd1534f00134c4e83127253bd77d5ae1
HeislerNarvaez <a16003022@alumnos.uady.mx>
2023-05-01 14:49:01 -0600

    subiendo cambios

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import os
 
-# Obtener todos los archivos dentro de la carpeta "Pages"
-pages = os.listdir("Pages")
+# Obtener todos los archivos dentro de la carpeta "Pages" que terminen en .py
+pages = [f[:-3] for f in os.listdir("Pages") if f.endswith(".py")]
 
 # Crear una lista con el nombre de cada página
 page_names = [page.split(".")[0] for page in pages]
 
 # Mostrar una lista de páginas en el sidebar
-selected_page = st.sidebar.radio("Selecciona una página", page_names)
+selected_page = st.sidebar.selectbox("Selecciona una página", page_names)
 
 # Importar y mostrar la página seleccionada
 page_path = os.path.join("Pages", f"{selected_page}.py")
