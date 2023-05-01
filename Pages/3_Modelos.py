91d57dc6d354a28a068f5aa34f7aae4134750fab
HeislerNarvaez <a16003022@alumnos.uady.mx>
2023-05-01 16:01:36 -0600

    cambios  modelo

--- Pages/3_Modelos.py
+++ Pages/3_Modelos.py
@@ -25,12 +25,10 @@ def run():
     # Entrenamiento del modelo seleccionado
     modelo = modelos[modelo_seleccionado]
     modelo.fit(x_train, y_train)
-    if modelo.score == 1:
+    score = modelo.score(x_test, y_test)
+    if score == 1.0:
         # Si el modelo se lo aprendió de memoria, vuelve a entrenar
         modelo.fit(x_train, y_train)
-        score = modelo.score(x_test, y_test)
-    else:
-        score = modelo.score(x_test, y_test)
 
     # Resultados del modelo
     st.write(f"El modelo {modelo_seleccionado} obtuvo una precisión de {score}")
